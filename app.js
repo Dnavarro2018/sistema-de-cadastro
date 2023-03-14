70f05d4b013839ce4c1fae86981e0d0a41309f18
First Last <you@mail.com>
2023-03-13 23:58:17 -0300

    subindo o arquivo correto de JS

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 console.log('Rodando sistema de gerenciamento de jogos')
 console.log('Aplicação está se conectando ao banco de dados...')
-
-=======
-console.log('Rodando sistema de gerenciamento de jogos')
-console.log('Aplicação está se conectando ao banco de dados...')
-
->>>>>>> a94b6699941b57b369392659f105930dd4a51a06
